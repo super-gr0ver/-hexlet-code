716663bece02181fe72b4e404655a874c3ec7eb5
Evgeny Dorodnov <e.dorodnov@gmail.com>
2022-11-24 01:14:49 +0300

    fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,47 @@
 /* eslint-disable import/prefer-default-export */
+import readlineSync from 'readline-sync';
 
 export const getCalc = () => {
-  const operators = ['+', '-', '*'];
-  const randomindexoperatos = Math.floor(Math.random(1, 3));
+  let count = '';
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = Math.trunc(Math.random() * 100);
+    const num2 = Math.trunc(Math.random() * 100);
+
+    const index = Math.trunc(Math.random() * 3);
+    const operators = ['+', '-', '*'];
+
+    console.log(`Question: ${num1} ${operators[index]} ${num2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    switch (operators[index]) {
+      case '+':
+        count = num1 + num2;
+        if (count === userAnswer) {
+          console.log('Correct!');
+        } else {
+          return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${count}'.\nLet's try again, !`);
+        }
+        break;
+      case '-':
+        count = num1 - num2;
+        if (count === userAnswer) {
+          console.log('Correct!');
+        } else {
+          return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${count}'.\nLet's try again, !`);
+        }
+        break;
+      case '*':
+        count = num1 * num2;
+        if (count === userAnswer) {
+          console.log('Correct!');
+        } else {
+          return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${count}'.\nLet's try again, !`);
+        }
+        break;
+      default:
+
+        break;
+    }
+  }
+  return count;
 };
