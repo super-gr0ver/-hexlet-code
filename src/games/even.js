9d3a2f454ad39f1396c3abd57ed5306a33b0c304
Evgeny Dorodnov <e.dorodnov@gmail.com>
2022-11-27 01:34:47 +0300

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -1,30 +1,23 @@
 /* eslint-disable import/prefer-default-export */
+import { getUserName, getAnswer, games } from '../index.js';
 
-import { getUsername, getAnswer } from '../index.js';
-
-const welcomeName = getUsername();
-console.log(`Hello, ${welcomeName}!`);
-
+getUserName();
 export const even = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const countRaund = 3;
 
-  for (let i = 1; i <= countRaund; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100);
+  const getQuestion = () => {
+    const randomNumber = Math.floor(Math.random() * 10);
     console.log(`Question: ${randomNumber}`);
+    let isEven = '';
+    isEven = (randomNumber % 2 === 0) ? isEven = 'yes' : isEven = 'no';
+    return isEven;
+  };
 
+  const getUserAnswer = () => {
     const userAnswer = getAnswer();
     if (userAnswer !== 'yes' && userAnswer !== 'no') {
       return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes' or 'no' .`);
-    }
-
-    let isEven = '';
-    isEven = (randomNumber % 2 === 0) ? isEven = 'yes' : isEven = 'no';
-    if (isEven === userAnswer) {
-      console.log('Correct!');
-    } else {
-      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven}'.\nLet's try again, ${welcomeName}!`);
-    }
-  }
-  return console.log(`Congratulations, ${welcomeName}!`);
+    } return userAnswer;
+  };
+  return games(getQuestion(), getUserAnswer());
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 /* eslint-disable import/prefer-default-export */
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-export const getUsername = () => {
+export const getUserName = () => {
+  console.log('Welcome to the Brain Games!');
   const welcomeName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${welcomeName}!`);
   return welcomeName;
 };
 
 export const getAnswer = () => {
   const userAnswer = readlineSync.question('Your answer: ');
   return userAnswer;
+};
+
+export const games = (question, answer, raund = 3) => {
+  for (let i = 0; i <= raund; i += 1) {
+    console.log(question);
+    if (question === answer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${question}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${getUserName()}!`);
+    }
+  }
+  return console.log(`Congratulations, ${getUserName()}!`);
 };
