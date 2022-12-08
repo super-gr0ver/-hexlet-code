c41685cb5c5b549aee1948c34a0ece101dc6f784
Evgeny Dorodnov <e.dorodnov@gmail.com>
2022-12-09 00:42:56 +0300

    fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,7 +3,16 @@ import getRandomInRange from '../randomizer.js';
 
 const gameDescription = () => 'What number is missing in the progression?';
 const gameTask = () => {
-
+  const arrLength = getRandomInRange(5, 10);
+  const randomIndex = getRandomInRange(0, 10);
+  console.log(randomIndex)
+  const arrResult = [];
+  for (let index = 0; index < arrLength; index += 1) {
+    arrResult.push(getRandomInRange(0, 10))
+  }
+  arrResult[randomIndex] = ".."
+  return arrResult;
+}
 };
 
 export default () => {
