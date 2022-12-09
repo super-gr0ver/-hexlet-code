f821f8a453d9ff4fc58306d18262df1d179cf125
Evgeny Dorodnov <e.dorodnov@gmail.com>
2022-12-09 21:14:01 +0300

    fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,14 +5,13 @@ const gameDescription = () => 'What number is missing in the progression?';
 const gameTask = () => {
   const arrLength = getRandomInRange(5, 10);
   const randomIndex = getRandomInRange(0, 10);
-  console.log(randomIndex)
+  console.log(randomIndex);
   const arrResult = [];
   for (let index = 0; index < arrLength; index += 1) {
-    arrResult.push(getRandomInRange(0, 10))
+    arrResult.push(getRandomInRange(0, 10));
   }
-  arrResult[randomIndex] = ".."
+  arrResult[randomIndex] = '..';
   return arrResult;
-
 };
 
 export default () => {
